4213f99a3f292188eae6f6006ee6c7e6b2dae9a2
原　涼馬 <fot39hara@gmail.com>
2021-11-21 01:58:15 +0900

    各デフォルト画面、ルーティング作成

--- resources/ts/components/pages/Login.tsx
+++ resources/ts/components/pages/Login.tsx
@@ -1,8 +1,18 @@
+import React from "react";
 import { ChangeEvent, memo, useState, VFC } from "react";
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import React from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const { login, loading } = useAuth();
+
+  const [userId, setUserId] = useState("");
+
+  const onClickLogin = () => login(userId);
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -13,16 +23,16 @@ export const Login: VFC = memo(() => {
         <Stack spacing={6} py={4} px={10}>
           <Input
             placeholder="ユーザID"
-            // value={userId}
-            // onChange={onChangeUserId}
+            value={userId}
+            onChange={onChangeUserId}
           />
-          {/* <PrimaryButton
+          <PrimaryButton
             disabled={userId === ""}
             loading={loading}
             onClick={onClickLogin}
-          > */}
-            {/* ログイン */}
-          {/* </PrimaryButton> */}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
--- resources/ts/router/Router.tsx
+++ resources/ts/router/Router.tsx
@@ -4,7 +4,9 @@ import { Redirect, Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { LoginUserProvider } from "../providers/LoginUserProvider";
+import { HomeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
   return (
@@ -19,6 +21,22 @@ export const Router: VFC = memo(() => {
         <Route exact path="/todo">
           <Login />
         </Route>
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {HomeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout Children={route.Children}></HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
       </LoginUserProvider>
       <Route path="*">
         <Page404 />
