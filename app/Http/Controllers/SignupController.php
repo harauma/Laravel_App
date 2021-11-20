38d6f718c1de3372b9871c1b861632ed7b9b0c7e
原　涼馬 <fot39hara@gmail.com>
2021-11-21 03:18:35 +0900

    インデント修正

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -7,34 +7,34 @@ use Illuminate\Http\Request;
 
 class SignupController extends Controller
 {
-	public function index(Request $request)
-	{
-		// $name = $request->input('name');
-		$accounts = Account::all();
-		return view('signup', ['accounts' => $accounts]);
-	}
+    public function index(Request $request)
+    {
+        // $name = $request->input('name');
+        $accounts = Account::all();
+        return view('signup', ['accounts' => $accounts]);
+    }
 
-	public function store(Request $request)
-	{
-		$validated = $request->validate([
-			'login_id' => 'required|min:4|max:255',
-			'password' => 'required|min:4|max:24',
-			'name' => 'required',
-			'age' => 'required|numeric',
-		]);
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'login_id' => 'required|min:4|max:255',
+            'password' => 'required|min:4|max:24',
+            'name' => 'required',
+            'age' => 'required|numeric',
+        ]);
 
-		$account = new Account();
+        $account = new Account();
 
-		$account->login_id = $request->input('login_id');
-		$account->password = $request->input('password');
-		$account->name = $request->input('name');
-		$account->age = $request->input('age');
+        $account->login_id = $request->input('login_id');
+        $account->password = $request->input('password');
+        $account->name = $request->input('name');
+        $account->age = $request->input('age');
 
-		$account->save();
+        $account->save();
 
-		// return view('signup', ['accounts' => Account::all()]);
-		return redirect()->route('signup');
-	}
+        // return view('signup', ['accounts' => Account::all()]);
+        return redirect()->route('signup');
+    }
 }
 // curl -X POST -H "Content-Type: application/json" -d '{"login_id":"abcdef","pasword":"aaa","name":"hoge","age":12345}' http://homestead.test/api/accounts
 // curl -X POST -H "Content-Type: application/json" -d '{"login_id":"hoge01", "password":"12345", "name":"あいうえお", "age":21}' http://homestead.test/api/accounts
