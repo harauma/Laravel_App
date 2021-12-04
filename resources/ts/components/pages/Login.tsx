af9bf3e392c87723e9d1baf130c7b9d6782b4d06
原　涼馬 <fot39hara@gmail.com>
2021-12-05 04:46:06 +0900

    アプリ名変更

--- resources/ts/components/pages/Login.tsx
+++ resources/ts/components/pages/Login.tsx
@@ -17,7 +17,7 @@ export const Login: VFC = memo(() => {
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
         <Heading as="h1" size="lg" textAlign="center">
-          ユーザー管理アプリ
+          Todoシェアアプリ
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
