c70a0f9f135a3a3f72921cac58a3fd874c119dee
原　涼馬 <fot39hara@gmail.com>
2021-12-07 02:14:44 +0900

    フロント PrimaryButton props追加

--- resources/ts/components/atoms/button/PrimaryButton.tsx
+++ resources/ts/components/atoms/button/PrimaryButton.tsx
@@ -6,15 +6,17 @@ type Props = {
   children: ReactNode;
   disabled?: boolean;
   loading?: boolean;
+  mr?: string;
   onClick: () => void;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children, disabled = false, loading = false, onClick } = props;
+  const { children, disabled = false, loading = false, mr, onClick } = props;
   return (
     <Button
       bg="teal.400"
       color="white"
+      mr={mr}
       _hover={{ opacity: 0.8 }}
       disabled={disabled || loading}
       isLoading={loading}
