fb18a8ba180808e154aaf7461603d75228132983
原　涼馬 <fot39hara@gmail.com>
2021-12-22 21:17:19 +0900

    タグ全件取得用カスタムフック作成

---
+++ resources/ts/hooks/useAllTags.ts
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react";
+import axios from "axios";
+
+import { Tag } from "../types/api/tag";
+import { useMessage } from "./useMessage";
+import { isEmpty } from "lodash";
+
+export const useAllTags = () => {
+  const { showMessage } = useMessage();
+
+  const [loading, setLoading] = useState(false);
+  const [tags, setTags] = useState<Array<Tag>>([]);
+
+  const getTags = useCallback(() => {
+    setLoading(loading);
+    async function getTags() {
+      await axios
+        .get<Array<Tag>>("http://homestead.test/api/tags")
+        .then((res) => {
+          if (isEmpty(res.data)) {
+            setTags([]);
+          } else {
+            setTags(res.data);
+          }
+        })
+        .catch(() =>
+          showMessage({ title: "タグの取得に失敗しました", status: "error" })
+        )
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+    getTags();
+  }, []);
+  return { getTags, loading, tags };
+};
