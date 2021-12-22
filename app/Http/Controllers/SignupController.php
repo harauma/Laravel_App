982c40f372e02df77a4b0658cadc348c51e62ded
原　涼馬 <fot39hara@gmail.com>
2021-12-22 21:02:28 +0900

    SignupController バリデーション追加

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -4,44 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class SignupController extends Controller
 {
-    public function index(Request $request)
-    {
-        // $name = $request->input('name');
-        $accounts = Account::all();
-        return view('signup', ['accounts' => $accounts]);
-    }
-
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'login_id' => 'required|min:4|max:255',
-            'password' => 'required|min:4|max:24',
-            'name' => 'required',
-            'age' => 'required|numeric',
-        ]);
-
-        $account = new Account();
-
-        $account->login_id = $request->input('login_id');
-        $account->password = $request->input('password');
-        $account->name = $request->input('name');
-        $account->age = $request->input('age');
-
-        $account->save();
-
-        // return view('signup', ['accounts' => Account::all()]);
-        return redirect()->route('signup');
-    }
 
     /**
      * ログイン処理
      */
     public function login(Request $request)
     {
+        $rulus = [
+            'login_id' => 'required',
+            'password' => 'required',
+        ];
+
+        $message = [
+            'login_id.required' => 'ユーザーIDを入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+
+        $validator = Validator::make($request->all(), $rulus, $message);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
         $loginId = $request->input('login_id');
         $password = $request->input('password');
         try {
@@ -59,5 +47,3 @@ class SignupController extends Controller
         }
     }
 }
-// curl -X POST -H "Content-Type: application/json" -d '{"login_id":"abcdef","pasword":"aaa","name":"hoge","age":12345}' http://homestead.test/api/accounts
-// curl -X POST -H "Content-Type: application/json" -d '{"login_id":"hoge01", "password":"12345", "name":"あいうえお", "age":21}' http://homestead.test/api/accounts
